29f5aa005848f1a340fcb854afb6f5c1fc6d55da
haiduong749 <haiduong749@gmail.com>
2022-04-18 20:39:29 +0700

    Thong tin

---
+++ Thongtin.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ADMIN
+#
+# Created:     18/04/2022
+# Copyright:   (c) ADMIN 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+print("Họ và tên: Lê Hải Dương")
+print("Lop hoc PYF2204R1")
