275d83d20d459df7397e6f934f9e68932cbb045e
haiduong749 <haiduong749@gmail.com>
2022-04-18 20:43:29 +0700

    displayInformation

---
+++ displayInformation.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ADMIN
+#
+# Created:     18/04/2022
+# Copyright:   (c) ADMIN 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+print("Le Hai Duong")
+print("https://github.com/haiduong749/demos")
+print("Lop hoc PYF2204R1")
