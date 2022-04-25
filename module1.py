27c923ffcf2f746ca21823aec3af7f3de63b8fd2
haiduong749 <haiduong749@gmail.com>
2022-04-25 21:51:44 +0700

    baitapnhietdo

---
+++ module1.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ADMIN
+#
+# Created:     25/04/2022
+# Copyright:   (c) ADMIN 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+c = input(" Enter temperature in Centigrade: ")
+
+f = (9*(int(c))/5)+32
+
+print(" Temperature in Fahrenheit is: ", f)
