c7bf5792289426a4e90d3d62ef93f4039982a660
haiduong749 <77055123+haiduong749@users.noreply.github.com>
2022-05-07 13:14:38 +0700

    Create tiente.py

---
+++ tiente.py
@@ -0,0 +1,9 @@
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+x = float(input("Nhap so tien can doi(USD): "))
+Y = float(input("Nhap ti gia: "))
+z = x * y 
+print("So tien quy doi la: ",z)
